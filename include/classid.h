9a7386861d135ea3aad01964c6ee5eeec6b998ff
Austin Loza <austinloza24@gmail.com>
2019-10-08 01:16:03 -0700

    Added some ignores to allow for vs as opposed to vs code

--- include/classid.h
+++ include/classid.h
@@ -1,9 +1,17 @@
 enum CharacterClass{
-    PHYSICSL,
+    PHYSICAL,
     MAGICAL,
     COMBO
 };
 
+
+/*
+Each enemy type will be associated
+with an integer value so that rng
+can determine what enemies randomly
+show up. This is specific to any 
+given area.
+*/
 enum EnemyType{
     SLIME = 0,
     DIRE_SLIME = 1,
